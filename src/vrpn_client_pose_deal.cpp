23dfcd4b2a5107627e7d178e39b01d7310870b7b
yuzhu222 <yuzhu_0222@sjtu.edu.cn>
2023-11-10 13:15:30 +0800

    update_v2

--- src/vrpn_client_pose_deal.cpp
+++ src/vrpn_client_pose_deal.cpp
@@ -8,11 +8,14 @@
 
 using namespace std;
 
-geometry_msgs::PoseStamped uav0_cur_pose, uav1_cur_pose;
-bool uav0_first_rece = true, uav1_first_rece = true;
+geometry_msgs::PoseStamped uav0_cur_pose, uav1_cur_pose, uav2_cur_pose, uav3_cur_pose, uav4_cur_pose;
+bool uav0_first_rece = true, uav1_first_rece = true, uav2_first_rece = true, uav3_first_rece = true, uav4_first_rece = true;
 
 double uav0_init_x = 0.0, uav0_init_y = 0.0, uav0_init_z = 0.0;
 double uav1_init_x = 0.0, uav1_init_y = 0.0, uav1_init_z = 0.0;
+double uav2_init_x = 0.0, uav2_init_y = 0.0, uav2_init_z = 0.0;
+double uav3_init_x = 0.0, uav3_init_y = 0.0, uav3_init_z = 0.0;
+double uav4_init_x = 0.0, uav4_init_y = 0.0, uav4_init_z = 0.0;
 
 int ctrl_rate = 30;
 
@@ -43,6 +46,45 @@ void uav1_vision_cb(const geometry_msgs::PoseStamped::ConstPtr& msg){
     uav1_cur_pose.pose.position.z = uav1_cur_pose.pose.position.z - uav1_init_z;
 }
 
+void uav2_vision_cb(const geometry_msgs::PoseStamped::ConstPtr& msg){
+    if (uav2_first_rece == true){
+        uav2_init_x = (*msg).pose.position.x;
+        uav2_init_y = (*msg).pose.position.y;
+        uav2_init_z = (*msg).pose.position.z;
+        uav2_first_rece = false;
+    }
+    uav2_cur_pose = *msg;
+    uav2_cur_pose.pose.position.x = uav2_cur_pose.pose.position.x - uav2_init_x;
+    uav2_cur_pose.pose.position.y = uav2_cur_pose.pose.position.y - uav2_init_y;
+    uav2_cur_pose.pose.position.z = uav2_cur_pose.pose.position.z - uav2_init_z;
+}
+
+void uav3_vision_cb(const geometry_msgs::PoseStamped::ConstPtr& msg){
+    if (uav3_first_rece == true){
+        uav3_init_x = (*msg).pose.position.x;
+        uav3_init_y = (*msg).pose.position.y;
+        uav3_init_z = (*msg).pose.position.z;
+        uav3_first_rece = false;
+    }
+    uav3_cur_pose = *msg;
+    uav3_cur_pose.pose.position.x = uav3_cur_pose.pose.position.x - uav3_init_x;
+    uav3_cur_pose.pose.position.y = uav3_cur_pose.pose.position.y - uav3_init_y;
+    uav3_cur_pose.pose.position.z = uav3_cur_pose.pose.position.z - uav3_init_z;
+}
+
+void uav4_vision_cb(const geometry_msgs::PoseStamped::ConstPtr& msg){
+    if (uav4_first_rece == true){
+        uav4_init_x = (*msg).pose.position.x;
+        uav4_init_y = (*msg).pose.position.y;
+        uav4_init_z = (*msg).pose.position.z;
+        uav4_first_rece = false;
+    }
+    uav4_cur_pose = *msg;
+    uav4_cur_pose.pose.position.x = uav4_cur_pose.pose.position.x - uav4_init_x;
+    uav4_cur_pose.pose.position.y = uav4_cur_pose.pose.position.y - uav4_init_y;
+    uav4_cur_pose.pose.position.z = uav4_cur_pose.pose.position.z - uav4_init_z;
+}
+
 
 int main(int argc, char** argv){
     
@@ -54,13 +96,23 @@ int main(int argc, char** argv){
             ("/uav0/mavros/vision/pose", 1, uav0_vision_cb);
     ros::Subscriber uav1_vision_sub = nh.subscribe<geometry_msgs::PoseStamped>
             ("/uav1/mavros/vision/pose", 1, uav1_vision_cb);
-
-   
+    ros::Subscriber uav2_vision_sub = nh.subscribe<geometry_msgs::PoseStamped>
+            ("/uav2/mavros/vision/pose", 1, uav2_vision_cb);
+    ros::Subscriber uav3_vision_sub = nh.subscribe<geometry_msgs::PoseStamped>
+            ("/uav3/mavros/vision/pose", 1, uav3_vision_cb);
+    ros::Subscriber uav4_vision_sub = nh.subscribe<geometry_msgs::PoseStamped>
+            ("/uav4/mavros/vision/pose", 1, uav4_vision_cb);
 
     ros::Publisher uav0_vision_pub = nh.advertise<geometry_msgs::PoseStamped>
             ("/uav0/mavros/vision_pose/pose", 1);
     ros::Publisher uav1_vision_pub = nh.advertise<geometry_msgs::PoseStamped>
             ("/uav1/mavros/vision_pose/pose", 1);
+    ros::Publisher uav2_vision_pub = nh.advertise<geometry_msgs::PoseStamped>
+            ("/uav2/mavros/vision_pose/pose", 1);
+    ros::Publisher uav3_vision_pub = nh.advertise<geometry_msgs::PoseStamped>
+            ("/uav3/mavros/vision_pose/pose", 1);
+    ros::Publisher uav4_vision_pub = nh.advertise<geometry_msgs::PoseStamped>
+            ("/uav4/mavros/vision_pose/pose", 1);
 
     ros::Rate ctrl_loop(ctrl_rate);
 
@@ -68,8 +120,14 @@ int main(int argc, char** argv){
 
         uav0_cur_pose.header.stamp = ros::Time::now();
         uav1_cur_pose.header.stamp = ros::Time::now();
+        uav2_cur_pose.header.stamp = ros::Time::now();
+        uav3_cur_pose.header.stamp = ros::Time::now();
+        uav4_cur_pose.header.stamp = ros::Time::now();
         uav0_vision_pub.publish(uav0_cur_pose);
         uav1_vision_pub.publish(uav1_cur_pose);
+        uav2_vision_pub.publish(uav2_cur_pose);
+        uav3_vision_pub.publish(uav3_cur_pose);
+        uav4_vision_pub.publish(uav4_cur_pose);
 
         ros::spinOnce();
         // spinner.spin();
